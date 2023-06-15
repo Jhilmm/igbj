30e5fbc8f8ece82519132ba508a07cbb9528ad43
Jhilm <jnukuravuisente28@gmail.com>
2023-06-15 10:05:55 -0400

    commit

--- app/controllers/personnel/PersonnelPositionController.php
+++ app/controllers/personnel/PersonnelPositionController.php
@@ -13,41 +13,6 @@ class PersonnelPositionController
     {
         include('app/views/person/person_item_update.php');
     }
-    public function assignItem($postData)
-    {
-
-        try {
-            if ($conn = get_connection()) {
-                $stmt = $conn->prepare("CALL DB_SP_Personal_add(?,?,?,1)");
-                $item = mysqli_real_escape_string($conn, $postData['item']);
-                $num_ci = mysqli_real_escape_string($conn, $postData['num_ci']);
-                $date = (new DateTime($postData['date']))->format('Y-m-d');
-                $stmt->bind_param(
-                    "sss",
-                    $item,
-                    $num_ci,
-                    $date
-                );
-                if ($stmt->execute()) {
-                    $response = array("message" => "Registro exitoso");
-                    $contentType = 'Content-Type: application/json';
-                    header($contentType);
-                    http_response_code(200);
-                    echo json_encode($response);
-                }
-            }
-        } catch (Exception $e) {
-            http_response_code(500);
-            echo json_encode(['error' => 'Error con la base de datos: ' . $e->getMessage()]);
-        } finally {
-            if (isset($conn)) {
-                $conn->close();
-            }
-            if (isset($stmt)) {
-                $stmt->close();
-            }
-        }
-    }
 
     public function assignPosition($postData)
     {
@@ -121,36 +86,6 @@ class PersonnelPositionController
         }
     }
 
-    function get_all_items($postData)
-    {
-        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-            http_response_code(405);
-            echo json_encode(['error' => 'Solicitud no válida']);
-            return;
-        }
-
-        try {
-            if ($conn = get_connection()) {
-                $query = "SELECT * FROM DB_VIEW_Item_Available_view";
-                $result = $conn->query($query);
-                if ($result = $conn->query($query)) {
-                    $rows = array();
-                    while ($row = $result->fetch_assoc()) {
-                        $rows[] = $row;
-                    }
-                    header('Content-Type: application/json');
-                    echo json_encode($rows);
-                }
-            }
-        } catch (Exception $e) {
-            http_response_code(500);
-            echo json_encode(['error' => 'Error con la base de datos: ' . $e->getMessage()]);
-        } finally {
-            if (isset($conn)) {
-                $conn->close();
-            }
-        }
-    }
     function hasPositionAssigned($postData)
     {
         try {
